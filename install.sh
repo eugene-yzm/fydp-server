c19687cdf67b82f3836900fe3c7948dcf6005626
Eugene M <symbato71@gmail.com>
2016-07-20 11:56:13 +0100

    first commit

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo apt-get update -y
+apt-get install -y build-essential python python-dev python-setuptools python-pip
+sudo apt-get install -y git
+sudo apt-get install -y libjpeg-dev libtiff-dev zlib1g-dev libfreetype6-dev liblcms2-dev
+pip install Django
