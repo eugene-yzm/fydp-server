204bf0f10ff86241549bc51db1d7bb7d8ebf5148
Eugene M <symbato71@gmail.com>
2017-02-05 12:08:55 +0000

    Major overhaul

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@ sudo apt-get update -y
 apt-get install -y build-essential python python-dev python-setuptools python-pip
 sudo apt-get install -y git
 sudo apt-get install -y libjpeg-dev libtiff-dev zlib1g-dev libfreetype6-dev liblcms2-dev
-pip install Django
+sudo pip install Django
+sudo pip install 
